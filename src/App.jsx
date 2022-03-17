e2a72c7d0093863765d00e8a37bb0591bf4dcf84
AnnomsDev <foysalbinnour@gmail.com>
2022-03-17 21:26:00 +0600

    initial react app with Vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <p>Recipe app</p>
+    </div>
+  )
+}
+
+export default App
